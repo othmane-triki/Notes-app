a0815293b9dda86be8b9fedcf1960d6e961e1b3d
othmane <trikiotmane@gmail.com>
2025-12-12 13:48:38 +0100

    a bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Notes-App">
-    <Routes>
-      <Route path="" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+    <App />
 )
